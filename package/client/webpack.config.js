e97d2d0dab2fff235ea25a4d6dfb671bb5e929e0
Amir Najafi <njfamirm@gmail.com>
2022-01-15 20:54:07 +0330

    feat(server): add deno config file

---
+++ package/client/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path")
+
+module.exports = {
+  mode: "production",
+  devtool: "eval-source-map",
+  entry: {
+    index: "./src/index.ts"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        include: [path.resolve(__dirname, "src")]
+      }
+
+    ]
+  },
+  resolve: {
+    extensions: [".ts", ".js"]
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "public", "js")
+  }
+}
