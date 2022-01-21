737b31d888a0afe7ef5ba3645f0c295442fcac04
Amir Najafi <njfamirm@gmail.com>
2022-01-21 12:19:47 +0330

    feat(client): add genrate-form element

---
+++ package/client/src/components/tweeph-genrate-form.ts
@@ -0,0 +1,74 @@
+import { html, css, LitElement } from 'lit';
+
+import './button.ts';
+
+export default class Input extends LitElement {
+  static styles? = css`
+  .input {
+    font-weight: 500;
+    font-size: 1em;
+    padding: 10px 25px;
+    user-select: none;
+    min-height: 40px;
+    color: rgb(15, 20, 25);
+    background-color: inherit;
+    border: none;
+    outline: none;
+    font-family: 'Roboto Mono', monospace;
+    display: inline;
+    border-radius: 500px;
+    flex-grow: 1;
+    background-color: #EFF3F4;
+  }
+
+  ::placeholder {
+    color: rgba(15, 20, 25, 0.4);
+  }
+
+  form {
+    width: 80vw;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    background-color: #EFF3F4;
+    align-items: center;
+    border-radius: 500px;
+    height: 100%;
+  }
+
+  .sumbit-btn {
+    font-weight: 600;
+    font-size: 1.2em;
+    border-radius: 500px;
+    padding: 10px 25px;
+    user-select: none;
+    min-height: 40px;
+    min-width: 36px;
+    background-color: rgb(29, 155, 240);
+    color: #fff;
+    border: none;
+    cursor: pointer;
+    font-family: 'Roboto Mono', monospace;
+    height: 100%;
+  }
+
+  .sumbit-btn:hover {
+    background-color: #1A8CD8;
+  }
+
+  .sumbit-btn:focus {
+    border: none;
+  }
+  `
+
+  render() {
+    return html`
+    <form action="genrate/"  id="linkform">
+        <input class="input" type="url" spellcheck="false" id="link-box" autocomplete="off" placeholder="Enter tweet link"></input>
+        <input class="sumbit-btn" type="submit" value="Genrate" />
+    </form>
+    `;
+  }
+}
+
+customElements.define('tweeph-genrate-form', Input);
