e9b47e8c75624fd1806e8ebbbb22f1c56f5339c2
Amir Najafi <njfamirm@gmail.com>
2022-02-01 23:16:59 +0330

    feat(client): add tweeph-paragraf component

---
+++ package/client/src/components/tweeph-paragraf.ts
@@ -0,0 +1,40 @@
+import { css, html, LitElement } from 'lit';
+import { property } from 'lit/decorators.js';
+
+export default class Paragraf extends LitElement {
+  static styles = css`
+    h2 {
+      font-weight: 700;
+      color: var(--blue-color);
+      user-select: none;
+      display: block;
+      font-size: 3em;
+      display: inline;
+    }
+
+    p {
+      padding-left: 1.3em;
+      color: var(--black-color);
+      font-size: 1.3em;
+      line-height: 1.5em;
+    }
+  `;
+
+  @property({ type: String, attribute: true })
+  header: string = '';
+
+  @property({ type: String, attribute: true })
+  id: string = '';
+
+  /**
+   * @TODO: add ID
+   */
+  render() {
+    return html`
+      <h2>${this.header}</h2>
+      <p>${this.innerHTML.toString()}</p>
+      `;
+  }
+}
+
+customElements.define('tweeph-paragraf', Paragraf);
--- package/client/src/page/tweeph-home-page.ts
+++ package/client/src/page/tweeph-home-page.ts
@@ -1,6 +1,7 @@
 import { css, html, LitElement } from 'lit';
 
 import '../components/tweeph-button.ts';
+import '../components/tweeph-paragraf.ts';
 
 export default class Body extends LitElement {
   static styles = css`
@@ -16,72 +17,126 @@ export default class Body extends LitElement {
     outline: none;
   }
 
+
+  p::selection {
+    color: var(--light-gray-color);
+    background-color: var(--black-color);
+  }
+
+
   .banner {
-    display: flex;
-    flex-direction: row;
-  }
-
-  .banner > * {
-    padding: 50px;
-  }
-
-  .text {
-    justify-content: center;
+    background-image: url("/public/img/banner-photo.webp");
+    background-repeat: no-repeat;
   }
 
   .banner-text {
-    margin: 1em;
-    padding-left: 0.2em;
-  }
-
-  .banner-text > * {
-    padding: 0 20px;
-  }
-
-  h1 {
-    color: var(--black-color);
-    font-size: 4em;
-    user-select: none;
-    font-weight: 700;
+    padding: 50px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    height: 82vh;
   }
 
   .text {
     display: flex;
     flex-direction: column;
+    margin: 1em;
+    padding-left: 0.2em;
   }
 
-  a {
-    align-self: end;
+  .text > p > span {
+    user-select: none;
+    color: var(--light-gray-color);
+    background-color: var(--dark-gray-color);
   }
 
-  img {
-    box-shadow: 5px 5px 12px 4px #00000024;
-    width: 30vw;
-    border-radius: 10px;
+  .text > p {
+    font-weight: 400;
+    color: var(--dark-gray-color)
+  }
+
+  h1 {
+    color: var(--black-color);
+    font-size: 5em;
+    user-select: none;
+    font-weight: 700;
+  }
+
+  .banner-text {
+    display: flex;
+    flex-direction: column;
   }
 
   p {
-    font-size: 1.3em;
-    line-height: 1.3em;
+    font-size: 1.5em;
+    line-height: 1.5em;
+  }
+
+  .generate-info {
+    padding: 2em 3em;
+    background-color: var(--light-gray-color);
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+  }
+  /**
+  * @TODO: fix typing effect
+  */
+  .generate-info > p > span {
+    /* width: 32ch;
+    animation: typing 4s steps(32);
+    white-space: nowrap;
+    overflow: hidden; */
+    color: var(--dark-gray-color)
+  }
+
+  /* @keyframes typing {
+    from {
+      width: 0
+    }
+  } */
+
+  .empty {
+    height: 75vh;
+  }
+
+  .info {
+    background-color: var(--light-gray-color);
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+    padding-top: 7em;
+  }
+
+  .info > * {
+    margin-bottom: 5em;
+    max-width: 70vw
   }
   `;
 
+  /**
+   * @TODO: fix width
+   */
   render() {
     return html`
     <div class="banner">
-      <div class="text">
+      <div class="banner-text">
         <h1>Tweeph</h1>
-        <div class="banner-text">
-          <p>Genrate customize able image from tweet</p>
-          <p>And share anywhere!</p>
+        <div class="text">
+          <p><span>Customize able</span> image from tweet</p>
+          <p>And share <span>anywhere!</span></p>
         </div>
-        <a href="/generate">
-          <tweeph-button>Generating</tweeph-button>
-        </a>
       </div>
-      <div class="photo">
-        <img src="/public/img/banner-photo.webp" alt="banner photo">
-
+      <div class="generate-info">
+        <p>There is no monetary plan<span>, in fact we do not have any plan!</span></p>
+        <tweeph-button>Starting</tweeph-button>
+      </div>
+      <div class="empty"></div>
+      <div class="info">
+        <tweeph-paragraf header="Simplity"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ullamcorper risus et turpis sagittis euismod. Maecenas interdum sed risus quis pretium. Sed id efficitur sem. Quisque pretium neque a lorem sollicitudin, at auctor libero facilisis. Ut iaculis, turpis sit amet tristique ornare, purus sem consequat ex, et placerat diam tortor sit amet nibh. Nulla facilisi. Nam ut elit at ipsum auctor rutrum. Phasellus laoreet, eros non mattis malesuada, est augue vehicula magna, id faucibus ante sapien vel urna. Integer in justo id arcu fringilla consequat. Maecenas ullamcorper justo nec arcu sodales lacinia. Aliquam dictum ipsum erat, eget vulputate mi pulvinar et. </tweeph-paragraf>
+        <tweeph-paragraf header="Free"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ullamcorper risus et turpis sagittis euismod. Maecenas interdum sed risus quis pretium. Sed id efficitur sem. Quisque pretium neque a lorem sollicitudin, at auctor libero facilisis. Ut iaculis, turpis sit amet tristique ornare, purus sem consequat ex, et placerat diam tortor sit amet nibh. Nulla facilisi. Nam ut elit at ipsum auctor rutrum. Phasellus laoreet, eros non mattis malesuada, est augue vehicula magna, id faucibus ante sapien vel urna. Integer in justo id arcu fringilla consequat. Maecenas ullamcorper justo nec arcu sodales lacinia. Aliquam dictum ipsum erat, eget vulputate mi pulvinar et. </tweeph-paragraf>
+        <tweeph-paragraf header="PWA"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ullamcorper risus et turpis sagittis euismod. Maecenas interdum sed risus quis pretium. Sed id efficitur sem. Quisque pretium neque a lorem sollicitudin, at auctor libero facilisis. Ut iaculis, turpis sit amet tristique ornare, purus sem consequat ex, et placerat diam tortor sit amet nibh. Nulla facilisi. Nam ut elit at ipsum auctor rutrum. Phasellus laoreet, eros non mattis malesuada, est augue vehicula magna, id faucibus ante sapien vel urna. Integer in justo id arcu fringilla consequat. Maecenas ullamcorper justo nec arcu sodales lacinia. Aliquam dictum ipsum erat, eget vulputate mi pulvinar et. </tweeph-paragraf>
       </div>
     </div>
     `;
