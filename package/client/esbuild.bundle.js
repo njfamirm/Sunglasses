02b92d9e375b509a7785281391f1267fe553f3f5
Amir Najafi <njfamirm@gmail.com>
2022-04-22 21:58:13 +0430

    feat(bundler): add buid prodution/source map mode

--- package/client/esbuild.bundle.js
+++
@@ -1,9 +0,0 @@
-const {build} = require('esbuild')
-const {dependencies, devDependencies} = require('./package.json')
-
-build({
-  entryPoints: ['src/index.ts'],
-  outdir: 'public/js/',
-  bundle: true,
-  // external: Object.keys(dependencies).concat(Object.keys(devDependencies)),
-})
