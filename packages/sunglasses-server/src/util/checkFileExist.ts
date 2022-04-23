6bdf268e33ed38498bd99379ceb9ecfd731605fc
Amir Najafi <njfamirm@gmail.com>
2022-04-23 15:40:07 +0430

    fix: fix file path

--- packages/sunglasses-server/src/util/checkFileExist.ts
+++ packages/sunglasses-server/src/util/checkFileExist.ts
@@ -2,7 +2,7 @@ import {Context} from 'https://deno.land/x/oak/mod.ts';
 
 // check file exist for serving static
 export async function checkFileExist(ctx: Context) {
-  const path = `${Deno.cwd()}/../client/${ctx.request.url.pathname}`;
+  const path = `${Deno.cwd()}/../sunglasses-web/${ctx.request.url.pathname}`;
   try {
     const fileInfo = await Deno.lstat(path);
     return fileInfo.isFile;
