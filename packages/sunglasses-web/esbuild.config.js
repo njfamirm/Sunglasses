7acb76aa71f47087e4bb9b2f3f03640e3aab000d
Amir Najafi <njfamirm@gmail.com>
2022-04-24 22:07:40 +0430

    feat(bundler): add esbuild config in js file

---
+++ packages/sunglasses-web/esbuild.config.js
@@ -0,0 +1,40 @@
+const esbuild = require('esbuild');
+
+// const browserList = ['chrome58', 'firefox57', 'safari11', 'edge16'];
+
+const DEBUG_MODE = (process.env.DEBUG_MODE === 'true') ? true : false;
+
+const esbuildOptionTypescript = {
+  entryPoints: ['src/sunglasses-pwa.ts'],
+  outfile: 'public/sunglasses-pwa.js',
+  bundle: true,
+  minify: true,
+  sourcemap: DEBUG_MODE,
+  // target: browserList,
+};
+
+const esbuildOptionCss = {
+  entryPoints: ['src/global.css'],
+  outfile: 'public/global.css',
+  bundle: true,
+  minify: true,
+  sourcemap: DEBUG_MODE,
+};
+
+esbuild
+    .build(esbuildOptionTypescript)
+    .then((msg) => {
+      console.log(`typescript: ${JSON.stringify(msg, null, 4)}`);
+    })
+    .catch((err) => {
+      console.log(`typescript error:' ${JSON.stringify(err, null, 4)}`);
+    });
+
+esbuild
+    .build(esbuildOptionCss)
+    .then((msg) => {
+      console.log(`typescript: ${JSON.stringify(msg, null, 4)}`);
+    })
+    .catch((err) => {
+      console.log(`css error:' ${JSON.stringify(err, null, 4)}`);
+    });
